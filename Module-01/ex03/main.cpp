55edef0517ff2e194a942fa9a168803e0acdcf67
Alexis <seith@synezia.com>
2021-12-18 20:38:28 +0100

    :hammer: fix: forgot HumanB

--- Module-01/ex03/main.cpp
+++ Module-01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 18:23:12 by amonteli          #+#    #+#             */
-/*   Updated: 2021/12/18 20:00:57 by amonteli         ###   ########lyon.fr   */
+/*   Updated: 2021/12/18 20:38:16 by amonteli         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,11 @@ int		main(void)
 	Elon.attack();
 	club.setType("some other type of club");
 	Elon.attack();
+
+	Weapon clubV2  = Weapon("crude spiked club");
+	HumanB Bezos("Bezos");
+	Bezos.setWeapon(clubV2);
+	Bezos.attack();
+	clubV2.setType("some other type of club");
+	Bezos.attack();
 }
