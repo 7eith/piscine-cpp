5de29d83d3caaf993a177dea56c0d36f137cd448
seith <seith@synezia.com>
2022-01-31 15:04:27 +0100

    :hammer: ref: forgot ref instead of PTR

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 18:23:12 by amonteli          #+#    #+#             */
-/*   Updated: 2021/12/18 18:27:48 by amonteli         ###   ########lyon.fr   */
+/*   Updated: 2022/01/31 15:04:12 by amonteli         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(void)
 	std::string& stringREF = String;
 
 	std::cout << "PTR - Address : " << stringPTR << std::endl;
-	std::cout << "REF - Address : " << stringREF << std::endl;
+	std::cout << "REF - Address : " << &stringREF << std::endl;
 	std::cout << "PTR - String : " << *stringPTR << std::endl;
 	std::cout << "REF - String : " << stringREF << std::endl;
 
