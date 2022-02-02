f947c978fa6c40bd33bc6d874a165a7a2f4adf2f
seith <seith@synezia.com>
2022-02-02 17:00:04 +0100

    :hammer: fix: problems...

--- Module-05/ex03/ShrubberyCreationForm.cpp
+++ Module-05/ex03/ShrubberyCreationForm.cpp
@@ -28,35 +28,23 @@ ShrubberyCreationForm &ShrubberyCreationForm::operator=(ShrubberyCreationForm co
 	return (*this);
 }
 
-void draw_arbre(std::string name)
-{
-    std::ofstream fichier(name + "_shrubbery", std::ios::out);
-        
-    if (fichier)
-    {
-        fichier << "\n\n🌤\n ";
-        fichier << "  ooxoxoo    ooxoo          ooxoxoo    ooxoo\n";
-        fichier << " ooxoxo oo  oxoxooo        ooxoxo oo  oxoxooo\n";
-        fichier << "oooo xxoxoo ooo ooox      oooo xxoxoo ooo ooox\n";
-        fichier << "oxo o oxoxo  xoxxoxo      oxo o oxoxo  xoxxoxo\n";
-        fichier << " oxo xooxoooo o ooo        oxo xooxoooo o ooo\n";
-        fichier << "   ooo\\oo\\  /o/o             ooo\\oo\\  /o/o\n";
-        fichier << "       \\  \\/ /                   \\  \\/ /\n";
-        fichier << "        |   /                     |   /\n";
-        fichier << "        |  |                      |  |\n";
-        fichier << "        | O|                      | O|\n";
-        fichier << "        |  |                      |  |\n";
-        fichier << "        |  |                      |  |\n";
-        fichier << "_______/    \\____🐷🐖__🐷🐖________/   \\______\n";
-        fichier.close();
-    }
-    else 
-        std::cerr << "Erreur à l'ouverture !" << std::endl;
-}
-
 void    ShrubberyCreationForm::execute(Bureaucrat const & executor) const
 {
-    if (this->getSign() == 0 || executor.getGrade() > this->getGexec())
-        throw Bureaucrat::GradeTooLowException();
-    draw_arbre(this->_target);
+	
+	if (this->getGexec() >= executor.getGrade() && this->getSign() == 1)
+	{
+		std::ofstream MonFichier;
+		MonFichier.open(std::string(this->_target + std::string("__shrubbery")).c_str());
+		MonFichier << "       _-_" << std::endl;
+	  	MonFichier << "    /~~   ~~\\" << std::endl;
+		MonFichier << " /~~   🍎     ~~\\" << std::endl;
+		MonFichier << "{  🍎         🍎 } " << std::endl;
+	 	MonFichier << " \\  _-     -_  /" << std::endl;
+		MonFichier << "   ~  \\\\ //  ~" << std::endl;
+		MonFichier << "_- -   | | _- _" << std::endl;
+	 	MonFichier << "  _ -  | |   -_" << std::endl;
+	 	MonFichier << "      // \\\\" << std::endl;
+
+		MonFichier.close();
+	}
 }
